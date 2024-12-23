4e83961486c7c2f9de18a703192c9d2f8e7d5f79
Hernán Espinoza <hernan.espinoza.dev@gmail.com>
2024-12-23 05:21:21 -0300

    conf vite par githubpages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import {fileURLToPath,URL} from 'node:url';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://HernanEspinozaDev.github.io/portfolio/',
+  
   plugins: [vue()],
-  resolve:{
-    alias:{
-      '@':fileURLToPath(new URL("./src",import.meta.url))
+  
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
