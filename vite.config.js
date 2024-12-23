cd4dca6cd6872be927f3f12407bce4321bc02b36
Hernán Espinoza <hernan.espinoza.dev@gmail.com>
2024-12-23 08:51:21 -0300

    conf para deploy vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://HernanEspinozaDev.github.io/portfolio/',
+  base: '/',
   
   plugins: [vue()],
   
